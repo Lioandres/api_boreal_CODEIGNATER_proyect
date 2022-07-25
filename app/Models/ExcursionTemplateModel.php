a7e5d2116eb669ffd9c78081820ca127589f7588
unknown <formacio@PC-1062606.formacio.local>
2022-07-25 13:26:29 +0200

    api boreal

---
+++ app/Models/ExcursionTemplateModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ExcursionTemplateModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'excursions_template';
+    protected $primaryKey       = 'id_excursion_template';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        "title",
+        "img",
+        "type",
+        "description",
+        "price_default",
+        "max_num_people_default"
+        
+
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
